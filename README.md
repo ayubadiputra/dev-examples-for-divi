0c66641c0e3dc6d86e41ea8c9bd98c5e5e9ba145
Ayub Adiputra <ayubna32@gmail.com>
2024-10-27 15:13:57 +0700

    feat(Core): introduce initial version of the plugin by adding main plugin file and update readme.

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# dev-examples-for-divi
+# Dev Examples for Divi
+
 Dev Examples for Divi is a WordPress plugin that provides code examples demonstrating how to use the Divi Builder API in third-party extensions. It is designed for demonstration, testing, and learning purposes.
+
+## Installation and Requirement
+
+After cloning the repository, activate the plugin. To enable certain examples, please check "Examples" section for more details.
+
+Dev Examples for Divi plugin's minimum requirements are WordPress 6.6 and PHP 7.2.
+
+## Examples
+
+To be added later ...
+
+## Contributing
+
+If you have requests, suggestions, or want to report bugs, [let me know](https://github.com/ayubadiputra/dev-examples-for-divi/issues).
---
+++ dev-examples-for-divi.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Plugin Name:       Dev Examples for Divi
+ * Plugin URI:        https://ayubna.work/plugins/dev-examples-for-divi
+ * Description:       Dev Examples for Divi is a WordPress plugin that provides code examples demonstrating how to use the Divi Builder API in third-party extensions. It is designed for demonstration, testing, and learning purposes.
+ * Version:           1.0.0
+ * Requires at least: 6.6
+ * Requires PHP:      7.2
+ * Author:            Ayub Adiputra
+ * Author URI:        https://ayubna.work
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain:       dev-examples-for-divi
+ * Domain Path:       /languages
+ */
+
+if ( ! defined( 'DED_DEV_EXAMPLES_FOR_DIVI_FILE' ) ) {
+	define( 'DED_DEV_EXAMPLES_FOR_DIVI_FILE', __FILE__ );
+}
